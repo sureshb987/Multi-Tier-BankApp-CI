2424b9eaf877016cc4d9644b84d472c1f689e546
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-18 20:07:36 +0530

    Update BankappApplication.java

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -1,15 +1,4 @@
-#package com.example.bankapp;
 
-#import org.springframework.boot.SpringApplication;
-#import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-#@SpringBootApplication
-#public class BankappApplication {
-
-	#public static void main(String[] args) {
-	#	SpringApplication.run(BankappApplication.class, args);
-	#}
-#}
 
 package com.example.bankapp;
 
