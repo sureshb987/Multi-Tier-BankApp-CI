d1248e2fa92281075890b3abb5d811147a6555de
sureshb987 <147478858+sureshb987@users.noreply.github.com>
2025-04-22 11:20:49 +0530

    Update BankappApplicationTests.java

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -7,6 +7,8 @@ import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+
 
 @Testcontainers
 @SpringBootTest
